a7354e32d4f0575ce5d3e1059b38bdc5ee389c15
drregg6 <dave.r.regg@gmail.com>
2023-02-12 18:10:32 -0500

    Actions and Outlets created

--- assignment-05/AmountView.swift
+++ assignment-05/AmountView.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class AmountView: UIViewController {
 
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var eurLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var inrLabel: UILabel!
+    var amount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- assignment-05/ViewController.swift
+++ assignment-05/ViewController.swift
@@ -10,12 +10,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var amount = 0
+    var eurSwitch = true
+    var gbpSwitch = true
+    var jpySwitch = true
+    var intSwitch = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func userInput(_ sender: UITextField) {
+    }
+    
+    
+    @IBAction func eurSlider(_ sender: UISwitch) {
+        if sender.isOn {
+            eurSwitch = true
+        } else {
+            eurSwitch = false
+        }
+    }
+    
+    
+    @IBAction func gbpSlider(_ sender: UISwitch) {
+        if sender.isOn {
+            gbpSwitch = true
+        } else {
+            gbpSwitch = false
+        }
+    }
+    
+    
+    @IBAction func jpySlider(_ sender: UISwitch) {
+        if sender.isOn {
+            jpySwitch = true
+        } else {
+            jpySwitch = false
+        }
+    }
+    
+    
+    @IBAction func inrSlider(_ sender: UISwitch) {
+        if sender.isOn {
+            intSwitch = true
+        } else {
+            intSwitch = false
+        }
+    }
+    
+    @IBAction func convertButton(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "toAmountView", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toAmountView" {
+            let destination = segue.destination as! AmountView
+            destination.amount = amount
+        }
+    }
 }
-
