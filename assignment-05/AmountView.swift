96aeb6c89d0532e396dc852ce4a3a9f0e869ea28
drregg6 <dave.r.regg@gmail.com>
2023-02-12 17:44:59 -0500

    Main view design complete

---
+++ assignment-05/AmountView.swift
@@ -0,0 +1,29 @@
+//
+//  AmountView.swift
+//  assignment-05
+//
+//  Created by Dave Regg on 2/12/23.
+//
+
+import UIKit
+
+class AmountView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- assignment-05/ViewController.swift
+++ assignment-05/ViewController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Dave Regg on 2/12/23.
 //
+//  To force int vals
+//  https://stackoverflow.com/questions/26919854/how-can-i-declare-that-a-text-field-can-only-contain-an-integer
 
 import UIKit
 
