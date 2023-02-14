d47f113f5d27273ab2956a2ff9f6c4c83d5634cd
drregg6 <dave.r.regg@gmail.com>
2023-02-14 17:16:16 -0500

    Stacks are hidden on AmountView unless the Sliders are set to true

--- assignment-05/AmountView.swift
+++ assignment-05/AmountView.swift
@@ -15,28 +15,42 @@ class AmountView: UIViewController {
     @IBOutlet weak var jpyLabel: UILabel!
     @IBOutlet weak var inrLabel: UILabel!
     
+    @IBOutlet weak var euroStack: UIStackView!
+    @IBOutlet weak var gbpStack: UIStackView!
+    @IBOutlet weak var jpyStack: UIStackView!
+    @IBOutlet weak var inrStack: UIStackView!
+    
     var convertLogic = ConvertLogic()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        euroStack.isHidden = true
+        gbpStack.isHidden = true
+        jpyStack.isHidden = true
+        inrStack.isHidden = true
+        
         // Do any additional setup after loading the view.
         print(convertLogic.amount)
         amountLabel.text = "$" + String(convertLogic.amount) + ".00"
         if (convertLogic.getEurSwitch()) {
             let amt = convertLogic.convertUsd(type: "eur")
             eurLabel.text = "€\(amt)"
+            euroStack.isHidden = false
         }
         if (convertLogic.getGbpSwitch()) {
             let amt = convertLogic.convertUsd(type: "gbp")
             gbpLabel.text = "£\(amt)"
+            gbpStack.isHidden = false
         }
         if (convertLogic.getJpySwitch()) {
             let amt = convertLogic.convertUsd(type: "jpy")
             jpyLabel.text = "¥\(amt)"
+            jpyStack.isHidden = false
         }
         if (convertLogic.getInrSwitch()) {
             let amt = convertLogic.convertUsd(type: "inr")
             inrLabel.text = "₹\(amt)"
+            inrStack.isHidden = false
         }
     }
 }
